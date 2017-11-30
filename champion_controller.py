a63754cb68bfe3fa2ab55aac52243e7c1ce0542d
Michael <mwang6@nd.edu>
2017-11-30 16:45:00 -0500

    champion and meta controller

--- champion_controller.py
+++ champion_controller.py
@@ -9,3 +9,35 @@ import re, json
 from _league_database import _league_database
 
 class ChampionController(object):
+	def __init__(self, ldb):
+        self.ldb  = ldb
+
+    def GET(self):
+        output = {}
+        output['result'] = 'success'
+
+        return json.dumps(output)
+
+    def GET_KEY(self, key):
+        output = {}
+        output['result'] = 'success'
+
+        return json.dumps(output)
+
+    def POST(self):
+    	req = cherrypy.request.body.read().decode()
+        req = json.loads(req)
+        output = {}
+
+        output['result'] = 'success'
+
+        return json.dumps(output)
+
+    def PUT_KEY(self, key):
+    	req = cherrypy.request.body.read().decode()
+        req = json.loads(req)
+        output = {}
+
+        output['result'] = 'success'
+
+        return json.dumps(output)
--- meta_controller.py
+++ meta_controller.py
@@ -9,3 +9,18 @@ import re, json
 from _league_database import _league_database
 
 class MetaController(object):
+
+	def __init__(self, ldb):
+        self.ldb  = ldb
+
+    def GET(self):
+        output = {}
+        output['result'] = 'success'
+
+        return json.dumps(output)
+
+    def GET_KEY(self, key):
+        output = {}
+        output['result'] = 'success'
+
+        return json.dumps(output)
