4414989cd6b1935722063af04b1518b3b2ea40a6
Michael <mwang6@nd.edu>
2017-12-07 03:40:00 -0500

    main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,38 +1,9 @@
-console.log("hi")
-// var top = new Button();
-// var mid = new Button();
-// var jungle = new Button();
-// var bot_sup = new Button();
-
-// var top_label = new Label();
-// var mid_label = new Label();
-// var jungle_label = new Label();
-// var bot_sup_label = new Label();
-
-// top_label.createLabel("The top lane", "top");
-// top.createButton("Top", "top");
-// mid.createButton("Mid", "mid");
-// jungle.createButton("Jungle", "jungle");
-// bot_sup.createButton("Bot/Sup", "bot-sup");
-
-// top.addClickEventHandler(get_top_meta);
-// mid.addClickEventHandler(get_mid_meta);
-// jungle.addClickEventHandler(get_jungle_meta);
-// bot_sup.addClickEventHandler(get_bot_sup_meta);
-
-// var main = new Format();
-// main.createFormat("main");
-// main.appendChild(top_label.getItem());
-
-// main.addToDocument();
-
 function get_top_meta() {
 	var request = new XMLHttpRequest();
 	request.open("GET", "http://student04.cse.nd.edu:51049/champion/15", true);
 
 	request.onload = function(e) {
 		var response = JSON.parse(request.responseText);
-		// top_label.setText(response["c_name"]);
 		console.log(response["c_name"])
 	}
 	request.send(null);
