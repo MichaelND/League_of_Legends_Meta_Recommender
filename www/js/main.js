004e1d3d67b104e91c2203be92d2cda2ea670563
Michael <mwang6@nd.edu>
2017-12-07 20:42:12 -0500

    main.js

--- www/js/main.js
+++ www/js/main.js
@@ -4,11 +4,15 @@ function get_meta() {
 
 	request.onload = function(e) {
 		var response = JSON.parse(request.responseText);
+		// var e = document.getElementById("sel1")
+		// var num = e.options[e.selectedIndex].value;
 		for (i = 0; i < 5; i++) {
+			document.getElementById("lane-row" + i).setAttribute("hidden", true)
 			document.getElementById("top" + i).src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + response["TOP"][i][0] + ".png"
 			// document.getElementById("jg" + i).src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + response["JUNGLE"][i][0] + ".png"
 			// document.getElementById("mid" + i).src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + response["MID"][i][0] + ".png"
 			// document.getElementById("bot" + i).src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + response["BOTTOM"][i][0] + ".png"
+			
 			// document.getElementById("top" + i).innerHTML = response["TOP"][i][0]
 		}
 	}
@@ -34,7 +38,7 @@ function post_meta_champ_id(champ_id, lane, meta_vote) {
 	}
 	request.send(null);
 }
-function get_champion_id(champ_id) {
+function get_champ_id(champ_id) {
 	var request = new XMLHttpRequest();
 	request.open("GET", "http://student04.cse.nd.edu:51049/champion/" + champ_id , true);
 
